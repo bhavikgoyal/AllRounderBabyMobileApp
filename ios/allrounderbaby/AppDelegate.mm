a83c581a2926d8bd9a9d0a0a4f884fa2c5934b1c
Avyak43 <Avyak43@AVYAK43>
2026-06-09 11:57:11 +0530

    Update FCM in IOS 09/06/2026

--- ios/allrounderbaby/AppDelegate.mm
+++ ios/allrounderbaby/AppDelegate.mm
@@ -1,16 +1,21 @@
 #import "AppDelegate.h"
 #import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"allrounderbaby";
+  // Initialize Firebase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 
+  self.moduleName = @"allrounderbaby";
   self.initialProps = @{};
 
-  // TEMPORARILY DISABLE FOR TESTING
+  // If you use react-native-splash-screen, uncomment:
   // [RNSplashScreen show];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
